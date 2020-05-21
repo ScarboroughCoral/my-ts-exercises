e489100fded1e28a8128f7a757b37fd60add2532
ScarboroughCoral <3249977074@qq.com>
2020-05-21 08:53:51 +0800

    feat:添加ts exercise 10

--- exercises/exercise-10/declarations/str-utils/index.d.ts
+++ exercises/exercise-10/declarations/str-utils/index.d.ts
@@ -1,4 +1,13 @@
 declare module 'str-utils' {
     // export const ...
     // export function ...
+    type strUtil = (input: string) => string;
+
+
+    export const strReverse: strUtil;
+    export const strToLower: strUtil;
+    export const strToUpper: strUtil;
+    export const strRandomize: strUtil;
+    export const strInvertCase: strUtil;
+
 }

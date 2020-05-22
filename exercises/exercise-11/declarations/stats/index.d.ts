31a2c12117585edb726dc99cf0ee54e1a80f8e1f
ScarboroughCoral <3249977074@qq.com>
2020-05-22 17:17:39 +0800

    feat:完成exercise 11

--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,3 +1,26 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    type Comparator<T> = (a: T, b: T) => number;
+    type GetIndexFunc = <T>(input: T[], comparator: Comparator<T>) => number;
+    type GetElementFunc = <T>(input: T[], comparator: Comparator<T>) => T;
+    export const getMaxIndex: GetIndexFunc;
+    export const getMaxElement: GetElementFunc;
+    export const getMinIndex: GetIndexFunc;
+    export const getMinElement: GetElementFunc;
+    export const getMedianIndex: GetIndexFunc;
+    export const getMedianElement: GetElementFunc;
+    export function getAverageValue<T, V>(input: T[], getValue: (input: T[]) => V): V;
+
 }
+// 我的菜鸡代码。。
+// declare module 'stats' {
+//     type Comparator<T> = (a: T, b: T) => number;
+//     type GetValue<T> = (input: T) => number;
+//     export function getMaxIndex<T>(input: T[], comparator: Comparator<T>): number;
+//     export function getMaxElement<T>(input: T[], comparator: Comparator<T>): T;
+//     export function getMinIndex<T>(input: T[], comparator: Comparator<T>): number;
+//     export function getMinElement<T>(input: T[], comparator: Comparator<T>): T;
+//     export function getMedianIndex<T>(input: T[], comparator: Comparator<T>): number;
+//     export function getMedianElement<T>(input: T[], comparator: Comparator<T>): T;
+//     export function getAverageValue<T>(input: T[], getValue: GetValue<T>): T;
+
+// }

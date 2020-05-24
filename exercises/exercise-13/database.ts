c28895116136c8b6b6e611ec9feb687591c81dae
ScarboroughCoral <3249977074@qq.com>
2020-05-24 11:14:36 +0800

    feat:完成exercise 13

--- exercises/exercise-13/database.ts
+++ exercises/exercise-13/database.ts
@@ -1,13 +1,21 @@
+type FieldQuery<T> = { $eq: T } | { $gt: T } | { $lt: T } | { $in: T[] };
+type Query<T extends {}> = {
+    [P in keyof T]?: FieldQuery<T[P]>;
+} & {
+    $text?: string;
+    $and?: Query<T>[];
+    $or?: Query<T>[];
+}
 export class Database<T> {
     protected filename: string;
-    protected fullTextSearchFieldNames: unknown[];
+    protected fullTextSearchFieldNames: string[];
 
-    constructor(filename: string, fullTextSearchFieldNames) {
+    constructor(filename: string, fullTextSearchFieldNames: string[]) {
         this.filename = filename;
         this.fullTextSearchFieldNames = fullTextSearchFieldNames;
     }
 
-    async find(query): Promise<T[]> {
+    async find(query: Query<T>): Promise<T[]> {
         return [];
     }
 }
